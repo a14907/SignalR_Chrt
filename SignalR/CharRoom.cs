2dd3b6cb50264c9b491457e4e68e229a96e54a32
a14907 <1490747123@qq.com>
2015-12-24 21:55:30 +0800

    SignalR实时聊天

---
+++ SignalR/CharRoom.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR
+{
+    public class CharRoom : Hub
+    {
+        private static List<UserMsg> allUser = new List<UserMsg>();
+        public List<UserMsg> SendAll(string fromName, string msg)
+        {
+            Clients.All.addMsg(fromName, msg);
+            return allUser;
+        }
+        public List<UserMsg> Send(string toId, string fromName, string msg)
+        {
+            var exist = allUser.FirstOrDefault(m => m.ConnectionId == toId);
+            if (exist == null)
+            {
+                Clients.Caller.addMsg("系统管理员", "该用户已下线");
+                return allUser;
+            }
+            if (exist.ConnectionId != Context.ConnectionId)
+            {
+                //存在那就发送
+                Clients.Client(toId).addMsg(fromName, msg);
+                Clients.Caller.addMsg(fromName, msg);
+            }
+            else
+            {
+                Clients.Client(toId).addMsg(fromName, msg);
+            }
+            return allUser;
+        }
+
+        public void FirstConnection(string name)
+        {
+            allUser.Add(new UserMsg
+            {
+                ConnectionId = Context.ConnectionId,
+                Name = name
+            });
+            Clients.All.AddUserList(allUser);
+            return;
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            allUser = allUser.FindAll(m => m.ConnectionId != Context.ConnectionId);
+            Clients.All.AddUserList(allUser);
+            return base.OnDisconnected(stopCalled);
+        }
+
+    }
+}
---
+++ SignalR/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalR.Startup))]
+namespace SignalR
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var hubConfiguration = new HubConfiguration();
+            hubConfiguration.EnableDetailedErrors = true;
+            hubConfiguration.EnableJSONP = true;
+            app.MapSignalR("/signalr", hubConfiguration);
+        }
+    }
+}
